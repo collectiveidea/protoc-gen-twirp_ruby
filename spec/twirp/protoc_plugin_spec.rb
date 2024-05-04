f54e42a464d0a59155f17f5592d199579ee0aa42
darronschall <darron.schall@gmail.com>
2024-05-04 15:17:34 -0400

    Remove initial template failing spec

--- spec/twirp/protoc_plugin_spec.rb
+++ spec/twirp/protoc_plugin_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Twirp::ProtocPlugin do
   it "has a version number" do
     expect(Twirp::ProtocPlugin::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
