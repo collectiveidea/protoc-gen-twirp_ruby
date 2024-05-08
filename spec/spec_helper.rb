1dc5a8ca7db6fcafb8777d6f12e6625aa9721403
darronschall <darron.schall@gmail.com>
2024-05-08 15:49:09 -0400

    Stand up our process spec with our new fixture.
    We still sip the example because we don't have it passing yet.
    
    Now that we have a proper fixture in place, we pave the way to move forward with fleshing out the generator itself.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "rspec/file_fixtures"
 require "twirp/protoc_plugin"
 
 RSpec.configure do |config|
--- spec/twirp/protoc_plugin/process_spec.rb
+++ spec/twirp/protoc_plugin/process_spec.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.describe Twirp::ProtocPlugin do
+  # Generate fixture:
+  #   `./spec/support/create_fixture -b -f service_code_gen_request_pb.bin ./spec/fixtures/service.proto`
+  let(:service_code_gen_request_pb) { fixture("service_code_gen_request_pb.bin").read }
+
   xit "generates expected output" do
-    # TODO: service_gen_request_pb = <string containing code gen request protobuf message>
-    response_pb = Twirp::ProtocPlugin.process(service_gen_request_pb)
+    response_pb = Twirp::ProtocPlugin.process(service_code_gen_request_pb)
     response = Google::Protobuf::Compiler::CodeGeneratorResponse.decode(response_pb)
+    # Match the output of the 1.10.0 go plugin when ran against the same service.proto.
+    # See: https://github.com/arthurnn/twirp-ruby/blob/1a653da2aec51723a403e6741dbebfefd38b0730/example/hello_world/service_twirp.rb
     expect(response.file.first.content).to eq <<~EOF
       require 'twirp'
       require_relative 'service_pb.rb'
