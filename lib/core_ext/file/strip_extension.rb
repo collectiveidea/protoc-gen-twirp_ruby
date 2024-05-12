b3c72f201a6bb127f4da97ba3e538e0a52b3a070
darronschall <darron.schall@gmail.com>
2024-05-12 13:21:51 -0400

    Refactor: Remove the file extension via `delete_suffix`
    Rather than use a regex that looks for the extension at the end of the string, prefer the `String#delete_suffix` built-in.

--- lib/core_ext/file/strip_extension.rb
+++ lib/core_ext/file/strip_extension.rb
@@ -6,6 +6,6 @@ class File
   # @return [String] the filename (preserving optional path) minus the file extension,
   #   e.g. "some/example/hello"
   def self.strip_extension(filename)
-    filename.sub(/#{File.extname(filename)}$/, "")
+    filename.delete_suffix(File.extname(filename))
   end
 end
